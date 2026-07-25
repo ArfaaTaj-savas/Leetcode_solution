8a1ab97e4cc6bb7b419d0e2e6c0151e02c8c5385
MD ARFAA TAJ (SAVAS) <arfaataj009@gmail.com>
2026-07-25 22:36:56 +0530

    Time: 3 ms (91.48%) | Memory: 46.5 MB (75.61%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    boolean[] visited;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+
+        visited = new boolean[nums.length];
+
+        backtrack(nums);
+
+        return ans;
+    }
+
+    private void backtrack(int[] nums) {
+        if (path.size() == nums.length) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i])
+                continue;
+
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
+                continue;
+
+            visited[i] = true;
+            path.add(nums[i]);
+
+            backtrack(nums);
+
+            path.remove(path.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
